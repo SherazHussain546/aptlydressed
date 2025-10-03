6a03e8ecf1812179a4c266c2766bb5267cdfcda3
SYNC TECH Solutions <synctechire@gmail.com>
2025-10-03 21:59:20 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8361455451-62132",
+  "appId": "1:753869429043:web:f81ce62682e7132321c646",
+  "apiKey": "AIzaSyB-qUbwywflCghp_Rnx2vZEI1YQzzzT6zo",
+  "authDomain": "studio-8361455451-62132.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "753869429043"
+};
