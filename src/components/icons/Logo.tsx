9e5601397d043fa9f9225037765ff36ad66e398c
SYNC TECH Solutions <synctechire@gmail.com>
2025-10-03 17:02:01 +0000

    change the text color of this pleae because black color is not visible (

--- src/components/icons/Logo.tsx
+++ src/components/icons/Logo.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export function Logo() {
   return (
-    <Link href="/" className="font-headline text-2xl font-bold tracking-tight text-foreground transition-colors hover:text-primary">
+    <Link href="/" className="font-headline text-2xl font-bold tracking-tight text-current transition-colors hover:text-primary">
       Aptly Dressed
     </Link>
   );
